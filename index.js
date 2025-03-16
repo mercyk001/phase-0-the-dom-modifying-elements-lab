ddd0716603d45b4c22d14a123b7527aae83d1f5e
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-03-16 15:51:06 +0300

    DOM editing

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Write your code here
+
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+    mainElement.remove();
+
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.textContent = "MERCY is the champion";
+document.body.appendChild(newHeader);
